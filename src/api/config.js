3127e246250e8587fcd8f213bc6c92db22769528
Isaac-Arauujo <isaac_arauujo@outlook.com>
2026-04-10 13:20:28 -0300

    ajuste final de config

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,11 @@ const rawApiBaseUrl =
   import.meta.env.VITE_API_BASE_URL ||
   'http://localhost:8080'
 
-export const API_BASE_URL = rawApiBaseUrl.replace(/\/$/, '')
+const normalizedApiBaseUrl = /^https?:\/\//i.test(rawApiBaseUrl)
+  ? rawApiBaseUrl
+  : `https://${rawApiBaseUrl}`
+
+export const API_BASE_URL = normalizedApiBaseUrl.replace(/\/$/, '')
 export const API_URL = `${API_BASE_URL}/api`
 
 export const buildAssetUrl = (path) => {
